d1419c7d1d248d657b8c7b51f0de8542dceb7262
Rupa Reddy <rupareddy@microsoft.com>
2022-01-25 11:08:10 +0530

    Delete centralus-swatest-node-16-main/centralus-swatest-node-16-main directory

--- centralus-swatest-node-16-main/centralus-swatest-node-16-main/api/GetMessage/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = async function (context, req) {
-  const date = "2022-01-01"
-  let text = process.version + "  " + date;
-  context.res = {
-    body: {
-      text: text
-    },
-  };
-};
